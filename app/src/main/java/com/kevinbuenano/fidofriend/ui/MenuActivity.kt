7344ef8ed0f9e579fc8dc2766b252ee2266d90e9
KevinB00 <kevbueeva@alu.edu.gva.es>
2023-05-03 22:17:35 +0200

    Diseñando Menu v.1

---
+++ app/src/main/java/com/kevinbuenano/fidofriend/ui/MenuActivity.kt
@@ -0,0 +1,12 @@
+package com.kevinbuenano.fidofriend.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kevinbuenano.fidofriend.R
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
