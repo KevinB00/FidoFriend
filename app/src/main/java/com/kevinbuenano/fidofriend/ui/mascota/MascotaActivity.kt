ff77865bbf4ff977308469fdc0c5fc73717dba2e
Usuario <kevbueeva@alu.edu.gva.es>
2023-05-15 14:33:54 +0200

    Creando menú de mascota

--- app/src/main/java/com/kevinbuenano/fidofriend/ui/mascota/MascotaActivity.kt
+++ app/src/main/java/com/kevinbuenano/fidofriend/ui/mascota/MascotaActivity.kt
@@ -1,12 +1,49 @@
 package com.kevinbuenano.fidofriend.ui.mascota
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.kevinbuenano.fidofriend.R
+import com.kevinbuenano.fidofriend.database.appDatabase
+import com.kevinbuenano.fidofriend.databinding.ActivityMascotaBinding
 
 class MascotaActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMascotaBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+    private lateinit var db: appDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mascota)
+        setContentView(ActivityMascotaBinding.inflate(layoutInflater).also { binding = it }.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.bottom_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.infoMascotaFragment,
+            R.id.historialMedicoFragment,
+            R.id.modificarMascotaFragment
+        )
+        )
+
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController)
+        NavigationUI.setupWithNavController(binding.toolbar, navController, appBarConfiguration)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item)
     }
 }
