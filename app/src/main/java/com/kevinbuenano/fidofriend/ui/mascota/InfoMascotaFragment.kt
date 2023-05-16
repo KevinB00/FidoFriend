84fdc2e007304bd0f70ec9af0ea0490168cb79ac
KevinB00 <kevbueeva@alu.edu.gva.es>
2023-05-17 00:35:54 +0200

    Interfaz mascota v.2

--- app/src/main/java/com/kevinbuenano/fidofriend/ui/mascota/InfoMascotaFragment.kt
+++ app/src/main/java/com/kevinbuenano/fidofriend/ui/mascota/InfoMascotaFragment.kt
@@ -1,16 +1,23 @@
 package com.kevinbuenano.fidofriend.ui.mascota
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.kevinbuenano.fidofriend.R
+import com.kevinbuenano.fidofriend.database.appDatabase
+import com.kevinbuenano.fidofriend.database.entities.MascotaEntity
+import com.kevinbuenano.fidofriend.database.repository.mascotaRepository
+import com.kevinbuenano.fidofriend.databinding.FragmentInfoMascotaBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 
 /**
  * A simple [Fragment] subclass.
@@ -18,43 +25,60 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class InfoMascotaFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentInfoMascotaBinding
+    private lateinit var db: appDatabase
+    private lateinit var repository: mascotaRepository
+    var idMascota: Int = 0
+    private lateinit var mascotaEntity: MascotaEntity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info_mascota, container, false)
+        binding = FragmentInfoMascotaBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment InfoMascotaFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            InfoMascotaFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mascotaActivity = activity as MascotaActivity
+        if (mascotaActivity != null) {
+            db = mascotaActivity.getDB()
+            idMascota = mascotaActivity.getMascotaId()
+        }
+        repository = mascotaRepository(db.mascotaDao())
+        cargar()
+
+
+
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        cargar()
+    }
+
+    private fun cargarActividad() {
+        when (mascotaEntity.actividad) {
+            "Muy activo" -> binding.imageView.setImageResource(R.drawable.cara_feliz)
+            "Sedentario" -> binding.imageView.setImageResource(R.drawable.cara_feliz)
+            "Moderado" -> binding.imageView.setImageResource(R.drawable.cara_feliz)
+        }
+    }
+
+    private fun cargar() {
+        viewLifecycleOwner.lifecycleScope.launch{
+            mascotaEntity = withContext(Dispatchers.IO){
+                 repository.getMascotaById(idMascota)
             }
+            binding.tViewNumeroPeso.text = "${mascotaEntity.peso} Kg"
+            val diferencia = binding.tViewDifNum.text.toString().toFloat() - mascotaEntity.peso
+            binding.tViewDifNum.text = diferencia.toString()
+            cargarActividad()
+
+        }
     }
 }
--- app/src/main/java/com/kevinbuenano/fidofriend/ui/mascota/ModificarMascotaFragment.kt
+++ app/src/main/java/com/kevinbuenano/fidofriend/ui/mascota/ModificarMascotaFragment.kt
@@ -1,14 +1,18 @@
 package com.kevinbuenano.fidofriend.ui.mascota
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.SeekBar
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.kevinbuenano.fidofriend.R
 import com.kevinbuenano.fidofriend.database.appDatabase
 import com.kevinbuenano.fidofriend.database.entities.MascotaEntity
 import com.kevinbuenano.fidofriend.database.repository.mascotaRepository
@@ -31,8 +35,13 @@ class ModificarMascotaFragment : Fragment() {
     private lateinit var db: appDatabase
     private lateinit var repository: mascotaRepository
     private lateinit var mascotaEntity: MascotaEntity
+    private lateinit var estadoMascota: String
+    private lateinit var actividadMascota: String
+    private lateinit var tamanyoMascota: String
+    private  var idMascota: Int = 0
     val estado = arrayOf("Sano", "Enfermo", "Sin revisión", "Obeso", "Cachorro")
     val actividad = arrayOf("Muy activo", "Sedentario", "Moderado")
+    val tamanyo = arrayOf("Pequeño", "Mediano", "Grande")
     private val mascotaEntityLiveData = MutableLiveData<MascotaEntity>()
 
 
@@ -49,6 +58,9 @@ class ModificarMascotaFragment : Fragment() {
         val spinnerActividad = binding.spinnerActividadMod
         val adapterActiviadad = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, actividad)
         spinnerActividad.adapter = adapterActiviadad
+        val spinnerTamanyo = binding.spinnerPorte
+        val adapterTamanyo = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, tamanyo)
+        spinnerTamanyo.adapter = adapterTamanyo
      return binding.root
     }
 
@@ -56,19 +68,19 @@ class ModificarMascotaFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mascotaActivity = activity as MascotaActivity
         db = mascotaActivity.getDB()
-        var idMascota = mascotaActivity.getMascotaId()
+        idMascota = mascotaActivity.getMascotaId()
         repository = mascotaRepository(db.mascotaDao())
         cargarMascota(idMascota)
 
         mascotaEntityLiveData.observe(viewLifecycleOwner) { mascota ->
             mascotaEntity = mascota
-            binding.seekPeso.progress = mascota.peso.toInt()
         }
 
         binding.seekPeso.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                peso = progress.toFloat()
-                binding.tViewPesoMascota.text = peso.toString()
+                val valor = progress - 13
+                peso = mascotaEntity.peso + valor
+                binding.tViewPesoMascota.text = valor.toString()
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {
@@ -78,8 +90,85 @@ class ModificarMascotaFragment : Fragment() {
            }
         }
         )
+        binding.spinnerEstadoMod.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                estadoMascota = estado[position]
+            }
 
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
 
+        }
+        binding.spinnerActividadMod.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                actividadMascota = actividad[position]
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
+        binding.spinnerPorte.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                tamanyoMascota = tamanyo[position]
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+        }
+        binding.btnModificar.setOnClickListener {
+            modificarMascota()
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        cargarMascota(idMascota)
+    }
+
+    private fun modificarMascota() {
+        try {
+            var mascota = MascotaEntity(
+                mascotaEntity.id_mascota,
+                mascotaEntity.nombre,
+                mascotaEntity.fecha_nacimiento,
+                peso,
+                estadoMascota,
+                mascotaEntity.edad,
+                actividadMascota,
+                tamanyoMascota,
+                mascotaEntity.perroGato,
+                mascotaEntity.usuario_id
+            )
+            binding.seekPeso.progress = 0
+            viewLifecycleOwner.lifecycleScope.launch {
+                withContext(Dispatchers.IO) {
+                    repository.updateMascota(mascota)
+                }
+                Toast.makeText(requireContext(), "Mascota modificada", Toast.LENGTH_LONG).show()
+                val bundle = Bundle()
+                findNavController().navigate(R.id.action_modificarMascotaFragment_to_infoMascotaFragment, bundle)
+            }
+        } catch (e: Exception) {
+            Toast.makeText(requireContext(), "Valores no válidos", Toast.LENGTH_LONG).show()
+
+        }
     }
 
     private fun cargarMascota(idMascota: Int) {
