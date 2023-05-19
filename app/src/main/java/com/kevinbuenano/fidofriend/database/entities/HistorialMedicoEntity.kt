4e808616f5752f104736ca62b2575a05eb39ce0f
Usuario <kevbueeva@alu.edu.gva.es>
2023-05-19 14:20:55 +0200

    Terminando adapter de HistorialMedicoAdapter + Ventana de creación de historial V.2

--- app/src/main/java/com/kevinbuenano/fidofriend/database/entities/HistorialMedicoEntity.kt
+++ app/src/main/java/com/kevinbuenano/fidofriend/database/entities/HistorialMedicoEntity.kt
@@ -4,6 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.ForeignKey.Companion.CASCADE
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "historial_mascota",
@@ -12,7 +13,13 @@ import androidx.room.PrimaryKey
         childColumns = ["mascota"],
         parentColumns = ["mascota_id"],
         onDelete = CASCADE
-    )])
+    )],
+    indices = arrayOf(
+        Index(
+            value = arrayOf("mascota"),
+            name = "idx_historial_mascota_mascota"
+        )
+    ))
 data class HistorialMedicoEntity(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "historial_mascota_id")
--- app/src/main/java/com/kevinbuenano/fidofriend/ui/mascota/nuevaVisita/NuevaVisitaActivity.kt
+++ app/src/main/java/com/kevinbuenano/fidofriend/ui/mascota/nuevaVisita/NuevaVisitaActivity.kt
@@ -1,28 +1,67 @@
 package com.kevinbuenano.fidofriend.ui.mascota.nuevaVisita
 
+import android.app.DatePickerDialog
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.lifecycleScope
 import com.kevinbuenano.fidofriend.R
+import com.kevinbuenano.fidofriend.database.appDatabase
 import com.kevinbuenano.fidofriend.database.entities.HistorialMedicoEntity
+import com.kevinbuenano.fidofriend.database.repository.historialMedicoRepository
 import com.kevinbuenano.fidofriend.databinding.ActivityNuevaVisitaBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.time.LocalDate
+import java.time.Period
+import java.time.format.DateTimeFormatter
+import java.util.Calendar
 
 class NuevaVisitaActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNuevaVisitaBinding
     private var fechaVisita: LocalDate? = null
     private var fechaVisitaString: String? = null
+    private lateinit var repository: historialMedicoRepository
+    private lateinit var db: appDatabase
     lateinit var historialVeterinario: HistorialMedicoEntity
     var idMascota: Int = 0
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nueva_visita)
         title = "Crear visita"
         idMascota = intent.getIntExtra("idMascota", 0)
+        db = appDatabase.getDatabase(applicationContext)
+        repository = historialMedicoRepository(db.historialMedicoDao())
+        val datePickerListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            // Convierte los valores de fecha seleccionados en un objeto LocalDate
+            fechaVisita = LocalDate.of(year, month + 1, dayOfMonth)
 
+            // Actualiza el texto del EditText con la fecha seleccionada
+            val formato = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+            fechaVisitaString = fechaVisita?.format(formato)
+            binding.eTextFechaVisita.setText(fechaVisita?.format(formato))
+        }
 
-    binding.btnAnyadirVisitaNueva.setOnClickListener{
+        binding.eTextFechaVisita.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(
+                this,
+                datePickerListener,
+                year,
+                month,
+                dayOfMonth
+            )
+            datePickerDialog.show()
+        }
+        binding.btnAnyadirVisitaNueva.setOnClickListener{
         anyadir()
     }
 
@@ -37,6 +76,12 @@ class NuevaVisitaActivity : AppCompatActivity() {
                 binding.eTextResumenInput.text.toString(),
                 idMascota
             )
+            lifecycleScope.launch(Dispatchers.IO){
+                repository.addHistorialMedico(historialVeterinario)
+                finish()
+
+            }
+
         }catch (e: Exception){
             Toast.makeText(this, "Los datos són incorrectos", Toast.LENGTH_SHORT).show()
         }
